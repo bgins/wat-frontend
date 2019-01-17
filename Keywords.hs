4cfbeab1842ecd5e00b12133c909eee725201510
Brian Ginsburg <brian@brianginsburg.com>
2019-01-16 16:42:33 -0800

    Add keywords, strings, identifiers, and parens
    Add tokens and rules for keywords, strings, identifiers and parens.
    Parse until the end of the file.
    Break integer rules into smaller pieces.
    Put keyword strings in a separate module. (There are so many!)
    Note that keywords do not work properly and some string cases are unhandled.

---
+++ Keywords.hs
@@ -0,0 +1,304 @@
+module Keywords ( keywords ) where
+
+
+keywords :: [String]
+keywords =
+    concat
+        [ types 
+        , instructions
+        , modules
+        ]
+
+
+
+--TYPES
+
+
+types :: [String]
+types =
+    concat
+        [ values
+        , functions
+        , tables
+        , globals
+        ]
+
+
+-- Section 6.4.1
+values :: [String]
+values =
+    [ "i32"
+    , "i64"
+    , "f32"
+    , "f64"
+    ]
+
+
+-- Section 6.4.3
+functions :: [String]
+functions =
+    [ "func"
+    , "param"
+    , "result"
+    ]
+
+
+-- Section 6.4.6
+tables :: [String]
+tables =
+    [ "funcref" ]
+
+
+-- Section 6.4.7
+globals :: [String]
+globals =
+    [ "mut" ]
+
+
+
+-- INSTRUCTIONS
+
+
+instructions :: [String]
+instructions =
+    concat
+        [ control
+        , parametric
+        , variable
+        , memory
+        , numeric
+        , folded
+        ]
+
+
+-- Section 6.5.2
+control :: [String]
+control =
+    [ "block"
+    , "loop"
+    , "if"
+    , "else"
+    , "end"
+    , "unreachable"
+    , "nop"
+    , "br"
+    , "br_if"
+    , "br_table"
+    , "return"
+    , "call"
+    , "call_indirect"
+    ]
+
+
+-- Section 6.5.3
+parametric :: [String]
+parametric =
+    [ "drop"
+    , "select"
+    ]
+
+
+-- Section 6.5.4
+variable :: [String]
+variable =
+    [ "local.get"
+    , "local.set"
+    , "local.tee"
+    , "global.get"
+    , "global.set"
+    ]
+
+
+-- Section 6.5.5
+-- TODO: the offset and align keywords include the number after the '='
+memory :: [String]
+memory =
+    [ "offset="
+    , "align="
+    , "i32.load"
+    , "i64.load"
+    , "f32.load"
+    , "f64.load"
+    , "i32.load8_s"
+    , "i32.load8_u"
+    , "i32.load16_s"
+    , "i32.load16_u"
+    , "i64.load8_s"
+    , "i64.load8_u"
+    , "i64.load16_s"
+    , "i64.load16_u"
+    , "i64.load32_s"
+    , "i64.load32_u"
+    , "i32.store"
+    , "i64.store"
+    , "f32.store"
+    , "f64.store"
+    , "i32.store8"
+    , "i32.store16"
+    , "i64.store8"
+    , "i64.store16"
+    , "i64.store32"
+    , "memory.size"
+    , "memory.grow"
+    ]
+
+
+-- Section 6.5.6
+numeric :: [String]
+numeric =
+    [ "i32.const"
+    , "i64.const"
+    , "f32.const"
+    , "f64.const"
+    , "i32.clz"
+    , "i32.ctz"
+    , "i32.popcnt"
+    , "i32.add"
+    , "i32.sub"
+    , "i32.mul"
+    , "i32.div_s"
+    , "i32.div_u"
+    , "i32.rem_s"
+    , "i32.rem_u"
+    , "i32.and"
+    , "i32.or"
+    , "i32.xor"
+    , "i32.shl"
+    , "i32.shr_s"
+    , "i32.shr_u"
+    , "i32.rotl"
+    , "i32.rotr"
+    , "i64.clz"
+    , "i64.ctz"
+    , "i64.popcnt"
+    , "i64.add"
+    , "i64.sub"
+    , "i64.mul"
+    , "i64.div_s"
+    , "i64.div_u"
+    , "i64.rem_s"
+    , "i64.rem_u"
+    , "i64.and"
+    , "i64.or"
+    , "i64.xor"
+    , "i64.shl"
+    , "i64.shr_s"
+    , "i64.shr_u"
+    , "i64.rotl"
+    , "i64.rotr"
+    , "f32.abs"
+    , "f32.neg"
+    , "f32.ceil"
+    , "f32.floor"
+    , "f32.trunc"
+    , "f32.nearest"
+    , "f32.sqrt"
+    , "f32.add"
+    , "f32.sub"
+    , "f32.mul"
+    , "f32.div"
+    , "f32.min"
+    , "f32.max"
+    , "f32.copysign"
+    , "f64.abs"
+    , "f64.neg"
+    , "f64.ceil"
+    , "f64.floor"
+    , "f64.trunc"
+    , "f64.nearest"
+    , "f64.sqrt"
+    , "f64.add"
+    , "f64.sub"
+    , "f64.mul"
+    , "f64.div"
+    , "f64.min"
+    , "f64.max"
+    , "f64.copysign"
+    , "i32.eqz"
+    , "i32.eq"
+    , "i32.ne"
+    , "i32.lt_s"
+    , "i32.lt_u"
+    , "i32.gt_s"
+    , "i32.gt_u"
+    , "i32.le_s"
+    , "i32.le_u"
+    , "i32.ge_s"
+    , "i32.ge_u"
+    , "i64.eqz"
+    , "i64.eq"
+    , "i64.ne"
+    , "i64.lt_s"
+    , "i64.lt_u"
+    , "i64.gt_s"
+    , "i64.gt_u"
+    , "i64.le_s"
+    , "i64.le_u"
+    , "i64.ge_s"
+    , "i64.ge_u"
+    , "f32.eq"
+    , "f32.ne"
+    , "f32.lt"
+    , "f32.gt"
+    , "f32.le"
+    , "f32.ge"
+    , "f64.eq"
+    , "f64.ne"
+    , "f64.lt"
+    , "f64.gt"
+    , "f64.le"
+    , "f64.ge"
+    , "i32.wrap_i64"
+    , "i32.trunc_f32_s"
+    , "i32.trunc_f32_u"
+    , "i32.trunc_f64_s"
+    , "i32.trunc_f64_u"
+    , "i64.extend_i32_s"
+    , "i64.extend_i32_u"
+    , "i64.trunc_f32_s"
+    , "i64.trunc_f32_u"
+    , "i64.trunc_f64_s"
+    , "i64.trunc_f64_u"
+    , "f32.convert_i32_s"
+    , "f32.convert_i32_u"
+    , "f32.convert_i64_s"
+    , "f32.convert_i64_u"
+    , "f32.demote_f64"
+    , "f64.convert_i32_s"
+    , "f64.convert_i32_u"
+    , "f64.convert_i64_s"
+    , "f64.convert_i64_u"
+    , "f64.promote_f32"
+    , "i32.reinterpret_f32"
+    , "i64.reinterpret_f64"
+    , "f32.reinterpret_i32"
+    , "f64.reinterpret_i64"
+    ]
+
+
+-- Section 6.5.7
+folded :: [String]
+folded =
+    [ "then" ]
+
+
+
+-- MODULES
+
+
+-- Section 6.6
+modules :: [String]
+modules =
+    [ "type"
+    , "import"
+    , "local"
+    , "table"
+    , "memory"
+    , "global"
+    , "export"
+    , "start"
+    , "elem"
+    , "offset"
+    , "data"
+    , "module"
+    ]
--- Lexer.hs
+++ Lexer.hs
@@ -1,9 +1,11 @@
+import Data.Char as Char
 import Numeric (readHex)
 import Prelude hiding (lex)
 import qualified Text.Parsec as Parsec
 import Text.Parsec ((<|>))
 import System.Directory
 
+import Keywords
 
 
 -- MAIN
@@ -11,14 +13,16 @@ import System.Directory
 
 main :: IO ()
 main = do
-  tests <- listDirectory "tests"
-  mapM_ runTest tests
+    tests <- listDirectory "tests/tokens"
+    mapM_ runTest tests
 
 
 runTest :: String -> IO ()
 runTest inputFile = do
     putStrLn $ id inputFile
-    text <- readFile $ "tests/" ++ inputFile
+    text <- readFile $ "tests/tokens/" ++ inputFile
+    putStr "  "
+    print text
     putStr "  "  -- does not print nicely for errors
     case Parsec.parse lex inputFile text of
         Left err  -> print err
@@ -29,12 +33,23 @@ runTest inputFile = do
 -- TOKENS
 
 
-data Token = UIntLit Int
+data Token = Keyword String
+           | UIntLit Int
            | SIntLit Int
+           | StringLit String
+           | Id String
+           | OpenParen
+           | CloseParen
+
 
 instance Show Token where
-  show (UIntLit n) = show n
-  show (SIntLit n) = show n
+    show (Keyword kw) = id kw
+    show (UIntLit n) = show n
+    show (SIntLit n) = show n
+    show (StringLit str) = show str
+    show (Id ident) = id ident
+    show OpenParen = show '('
+    show CloseParen = show ')'
 
 
 
@@ -43,40 +58,140 @@ instance Show Token where
 
 lex :: Parsec.Parsec String () [Token]
 lex = do
-  intLitToken <- integer
-  return [intLitToken]
+    Parsec.manyTill token Parsec.eof
 
 
-integer :: Parsec.Parsec String () Token
-integer = do
-    Parsec.char '+'
-    sN <- hexnum <|> num
-    return (SIntLit sN)
-  <|> do
-    Parsec.char '-'
-    sN <- hexnum <|> num
-    return (SIntLit (- sN))
-  <|> do
+token :: Parsec.Parsec String () Token
+token = do
+    token <- Parsec.choice
+        [ keyword
+        , unsignedInteger
+        , signedInteger
+        , string
+        , identifier
+        , openParen
+        , closeParen
+        ]
+    Parsec.spaces
+    return token
+
+
+
+-- INTEGER LITERALS
+
+
+unsignedInteger :: Parsec.Parsec String () Token
+unsignedInteger = do
     uN <- hexnum <|> num
     return (UIntLit uN)
 
 
+signedInteger :: Parsec.Parsec String () Token
+signedInteger = do
+    positiveInteger <|> negativeInteger
+
+
+positiveInteger :: Parsec.Parsec String () Token
+positiveInteger = do
+    Parsec.char '+'
+    sN <- hexnum <|> num
+    return (SIntLit sN)
+
+
+negativeInteger :: Parsec.Parsec String () Token
+negativeInteger = do
+    Parsec.char '-'
+    sN <- hexnum <|> num
+    return (SIntLit (- sN))
+
+
 hexnum :: Parsec.Parsec String () Int
 hexnum = do
-  Parsec.string "0x"
-  n <- removeUnderscores <$> Parsec.many1 (Parsec.hexDigit <|> Parsec.char '_')
-  return (fst $ head $ readHex n)
+    Parsec.string "0x"
+    n <- removeUnderscores <$> Parsec.many1 (Parsec.hexDigit <|> Parsec.char '_')
+    return (fst $ head $ readHex n)
 
 
 num :: Parsec.Parsec String () Int
 num = do
-  n <- removeUnderscores <$> Parsec.many1 (Parsec.digit <|> Parsec.char '_')
-  return (read n :: Int)
-
-
-
--- UTILITY
+    n <- removeUnderscores <$> Parsec.many1 (Parsec.digit <|> Parsec.char '_')
+    return (read n :: Int)
 
 
 removeUnderscores :: String -> String
-removeUnderscores = filter (not . (`elem` "_" ))
+removeUnderscores =
+    filter (not . (`elem` "_" ))
+
+
+
+
+-- STRING LITERALS
+
+
+string :: Parsec.Parsec String () Token
+string = do
+    str <- betweenQuotes (concat <$> Parsec.many stringElement)
+    return (StringLit str)
+
+
+stringElement :: Parsec.Parsec String () String
+stringElement = do
+    elem <- escapeSequence <|> stringChar
+    return elem
+
+
+-- TODO: Add raw bytes and additional unicode characters
+escapeSequence :: Parsec.Parsec String () String
+escapeSequence = do
+    escape <- Parsec.char '\\'
+    char <- Parsec.oneOf "tnr\"`\\"
+    return [escape, char]
+
+
+-- TODO: Prohibit U+7F and <= U+20?
+stringChar :: Parsec.Parsec String () String
+stringChar = do
+    char <- Parsec.noneOf "\\\""
+    return [char]
+
+
+betweenQuotes :: Parsec.Parsec String () String -> Parsec.Parsec String () String
+betweenQuotes =
+    Parsec.between (Parsec.char '"') (Parsec.char '"')
+
+
+
+-- IDENTIFIERS
+
+
+identifier :: Parsec.Parsec String () Token
+identifier = do
+    marker <- Parsec.char '$'
+    name <- Parsec.many1 $ Parsec.satisfy Char.isAscii
+    return (Id (marker:name))
+
+
+
+-- PARENS
+
+
+openParen :: Parsec.Parsec String () Token
+openParen = do
+    Parsec.char '('
+    return OpenParen
+
+
+closeParen :: Parsec.Parsec String () Token
+closeParen = do
+    Parsec.char ')'
+    return CloseParen
+
+
+
+-- KEYWORDS
+
+
+keyword :: Parsec.Parsec String () Token
+keyword = do
+    kw <- Parsec.choice $ map (Parsec.try . Parsec.string) Keywords.keywords
+    return (Keyword kw)
